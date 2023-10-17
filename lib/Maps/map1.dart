24ad70ed8693cdce59bdb4e27a5acedd8895c300
muhammadfurqan2002 <m.furqan2002@gmail.com>
2023-10-18 01:41:20 +0500

    updated

---
+++ lib/Maps/map1.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Map_1 extends StatefulWidget {
+  const Map_1({super.key});
+
+  @override
+  State<Map_1> createState() => _Map_1State();
+}
+
+class _Map_1State extends State<Map_1> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _currentPosition = const CameraPosition(
+    target: LatLng(32.1877, 74.1945),
+    zoom: 13,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        body: GoogleMap(
+          initialCameraPosition: _currentPosition,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+}
